0d58ebe4f7e76b65a78c38d448c3de8098a3d60e
paynecrl97 <chris.payne@bedegaming.com>
2015-06-28 17:38:13 +0100

    Adds Glimpse decorator for Content Manager

---
+++ src/Orchard.Web/Modules/Orchard.Glimpse/TimelineCategories.cs
@@ -0,0 +1,15 @@
+﻿using Glimpse.Core.Message;
+
+namespace Orchard.Glimpse
+{
+    public class TimelineCategories 
+    {
+        public static TimelineCategoryItem Cache { get { return new TimelineCategoryItem("Cache", "#154B87", "#154B87"); } }
+        public static TimelineCategoryItem Layers { get { return new TimelineCategoryItem("Layers", "#E6261D", "#E6261D"); } }
+        public static TimelineCategoryItem ContentManager { get { return new TimelineCategoryItem("Content Manager", "#603182", "#603182"); } }
+        public static TimelineCategoryItem Drivers { get { return new TimelineCategoryItem("Drivers", "#9e4058", "#9e4058"); } }
+        public static TimelineCategoryItem Shapes { get { return new TimelineCategoryItem("Shapes", "#bcbb27", "#bcbb27"); } }
+        public static TimelineCategoryItem Widgets { get { return new TimelineCategoryItem("Widgets", "#da7520", "#da7520"); } }
+
+    }
+}
