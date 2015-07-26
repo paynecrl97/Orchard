13c9ea729e7a74c91723a8a4a01c2cf031f01f7f
paynecrl97 <chris.payne@bedegaming.com>
2015-07-26 13:57:19 +0100

    Adds content part tab

---
+++ src/Orchard.Web/Modules/Orchard.Glimpse/AlternateImplementation/GlimpseContentPartDriverDecorator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Glimpse.Services;
+using Orchard.Glimpse.Tabs.PartDrivers;
+
+namespace Orchard.Glimpse.AlternateImplementation
+{
+    [OrchardDecorator]
+    public class GlimpseContentPartDriverDecorator : IContentPartDriver
+    {
+        private readonly IContentPartDriver _decoratedService;
+        private readonly IGlimpseService _glimpseService;
+
+        public GlimpseContentPartDriverDecorator(IContentPartDriver decoratedService, IGlimpseService glimpseService) {
+            _decoratedService = decoratedService;
+            _glimpseService = glimpseService;
+        }
+
+        public DriverResult BuildDisplay(BuildDisplayContext context) {
+            _glimpseService.PublishTimedAction(() => _decoratedService.BuildDisplay(context), (r, t) => {
+                    string stereotype;
+                    if (!context.ContentItem.TypeDefinition.Settings.TryGetValue("Stereotype", out stereotype))
+                    {
+                        stereotype = "Content";
+                    }
+
+                    return new PartDriverMessage {
+                        Stereotype = stereotype,
+                        Context = context,
+                        Duration = t.Duration
+                    };
+                },
+                TimelineCategories.Layers, "Build Display");
+
+            Trace.WriteLine("Driver Build Display ");
+            return _decoratedService.BuildDisplay(context);
+        }
+
+        public DriverResult BuildEditor(BuildEditorContext context) {
+            return _decoratedService.BuildEditor(context);
+        }
+
+        public DriverResult UpdateEditor(UpdateEditorContext context) {
+            return _decoratedService.UpdateEditor(context);
+        }
+
+        public void Importing(ImportContentContext context) {
+            _decoratedService.Importing(context);
+        }
+
+        public void Imported(ImportContentContext context) {
+            _decoratedService.Imported(context);
+        }
+
+        public void Exporting(ExportContentContext context) {
+            _decoratedService.Exporting(context);
+        }
+
+        public void Exported(ExportContentContext context) {
+            _decoratedService.Exported(context);
+        }
+
+        public IEnumerable<ContentPartInfo> GetPartInfo() {
+            return _decoratedService.GetPartInfo();
+        }
+
+        public void GetContentItemMetadata(GetContentItemMetadataContext context) {
+            _decoratedService.GetContentItemMetadata(context);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Glimpse/Tabs/PartDrivers/PartDriverMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Glimpse.Core.Message;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Glimpse.Models;
+
+namespace Orchard.Glimpse.Tabs.PartDrivers {
+    public class PartDriverMessage : MessageBase, IDurationMessage
+    {
+        public string Stereotype { get; set; }
+        public BuildDisplayContext Context { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
