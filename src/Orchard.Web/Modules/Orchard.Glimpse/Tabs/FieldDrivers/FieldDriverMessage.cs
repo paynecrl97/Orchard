cdbddebd65aa699c1a441d12f98558b7c35d9f8d
paynecrl97 <chris.payne@bedegaming.com>
2015-07-26 14:48:33 +0100

    Fields WIP

---
+++ src/Orchard.Web/Modules/Orchard.Glimpse/Tabs/FieldDrivers/FieldDriverMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Glimpse.Core.Message;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Glimpse.Models;
+
+namespace Orchard.Glimpse.Tabs.FieldDrivers {
+    public class FieldDriverMessage : MessageBase, IDurationMessage
+    {
+        public string Stereotype { get; set; }
+        public BuildDisplayContext Context { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Glimpse/Tabs/FieldDrivers/FieldDriversTab.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Extensions;
+
+namespace Orchard.Glimpse.Tabs.FieldDrivers {
+    public class FieldDriversTab : TabBase, ITabSetup, IKey, ILayoutControl {
+        public override object GetData(ITabContext context) {
+            var messages = context.GetMessages<FieldDriverMessage>().ToList();
+
+            if (!messages.Any()) {
+                return "There have been no Content Part Field events recorded. If you think there should have been, check that the 'Glimpse for Orchard Content Part Fields' feature is enabled.";
+            }
+
+            return messages;
+        }
+
+        public override string Name {
+            get { return "Drivers (Fields)"; }
+        }
+
+        public void Setup(ITabSetupContext context) {
+            context.PersistMessages<FieldDriverMessage>();
+        }
+
+        public string Key {
+            get { return "glimpse_orchard_partfields"; }
+        }
+
+        public bool KeysHeadings { get { return false; } }
+    }
+}
